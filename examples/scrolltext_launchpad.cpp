74c23981e2739cc1868ba389189502cdfdc932ca
Limitz <eddy@splendo.nl>
2014-03-18 22:32:11 +0100

    Scroll test example

---
+++ examples/scrolltext_launchpad.cpp
@@ -0,0 +1,25 @@
+#include <novation/launchpad.h>
+
+using namespace Novation;
+
+int main(int argc, char *argv[])
+{
+	LaunchPad *launchpad = 0;
+
+	try
+	{
+		launchpad = new LaunchPad(argc > 1 ? argv[1] : "hw:1,0,0");
+		const char *text = argc > 2 ? argv[2] : "Limitz Launchpad Library";
+		
+		launchpad->ScrollText(text, PadColor::Lime, false);
+		printf("Press enter to continue...");
+	
+		getchar();
+	}
+	catch (Exception &e)
+	{
+		fprintf(stderr, "%s\n", e.Message());
+	}
+	delete launchpad;
+	return 0;
+}
